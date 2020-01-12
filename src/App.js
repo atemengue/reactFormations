8334a6d424c2111085f4d5480091383c00666128
atemengue <moafembe@gmail.com>
2020-01-12 11:49:36 +0100

    Unmounting and Remounting Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import StarMatch from "./StartMatch";
+import StartMatch from "./StartMatch";
 
 function App() {
   return (
     <div className="App">
-      <StarMatch />
+      <StartMatch />
     </div>
   );
 }
