4d622f089a0754f1e7a4ea874a90122cfc0adccf
atemengue <moafembe@gmail.com>
2020-01-12 08:20:37 +0100

    add  form and user ReactRef

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import CardList from "./CardList";
+import data from "./data";
+import Form from "./Form";
 
 function App() {
   return (
     <div className="App">
       <h1 className="header">The Github Card</h1>
       <div className="container">
-        <CardList />
+        <Form />
+        <CardList profiles={data} />
       </div>
     </div>
   );
