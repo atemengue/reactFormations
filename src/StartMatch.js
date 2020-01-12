c2127b8a7ebaa166447b00066362149c81683f26
atemengue <moafembe@gmail.com>
2020-01-12 10:49:23 +0100

    05 Behavior Functions- State =- New State

--- src/StartMatch.js
+++ src/StartMatch.js
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { utils } from "./utilis";
+import PlayNumber from "./PlayNumber";
+import StarsDisplay from "./StarsDisplay";
 
 const StarMatch = () => {
+  const [stars, setStars] = useState(utils.random(1, 9));
+  const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+  const [candidateNums, setCandidateNums] = useState([]);
+
+  const candidateAreWrong = utils.sum(candidateNums) > stars;
+
+  const numberStatus = number => {
+    if (!availableNums.includes(number)) {
+      return "used";
+    }
+    if (candidateNums.includes(number)) {
+      return candidateAreWrong ? "wrong" : "candidate";
+    }
+
+    return "available";
+  };
+
+  const onNumberClick = (number, status) => {
+    console.log(number, status);
+    if (status === "used") {
+      return "";
+    }
+
+    const newCandidateNums =
+      status === "available"
+        ? candidateNums.concat(number)
+        : candidateNums.filter(cn => cn !== number);
+
+    if (utils.sum(newCandidateNums) !== stars) {
+      setCandidateNums(newCandidateNums);
+    } else {
+      const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+      );
+      setStars(utils.randomSumIn(newAvailableNums, 9));
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
+    }
+  };
+
   return (
     <div className="game">
       <div className="help">
@@ -8,26 +51,17 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
+          <StarsDisplay count={stars} />
         </div>
         <div className="right">
-          <button className="number">1</button>
-          <button className="number">2</button>
-          <button className="number">3</button>
-          <button className="number">4</button>
-          <button className="number">5</button>
-          <button className="number">6</button>
-          <button className="number">7</button>
-          <button className="number">8</button>
-          <button className="number">9</button>
+          {utils.range(1, 9).map(number => (
+            <PlayNumber
+              onClick={onNumberClick}
+              status={numberStatus(number)}
+              key={number}
+              number={number}
+            />
+          ))}
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
