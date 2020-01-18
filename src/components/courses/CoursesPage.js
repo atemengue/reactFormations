3300d76b80bc80e1bdcfaefb8c222164afc5618a
atemengue <moafembe@gmail.com>
2020-01-18 14:20:39 +0100

    add mapStateToProps and dispatch form one

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,34 +1,57 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { createCourse } from "../../redux/actions/courseActions";
 
 class CoursesPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      course: {
-        title: ""
-      }
-    };
-  }
+  state = {
+    course: {
+      title: ""
+    }
+  };
 
-  handleChange(event) {
+  handleChange = event => {
     const course = { ...this.state.course, title: event.target.value };
     this.setState({ course });
-  }
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.createCourse(this.state.course);
+  };
   render() {
     return (
       <div>
         <h2>Courses</h2>
-        <h3>Add Courses</h3>
-        <form>
+        <h3>Add Course</h3>
+        <form onSubmit={this.handleSubmit}>
           <input
             type="text"
             value={this.state.course.title}
             onChange={this.handleChange}
           />
+          {this.props.courses.map(course => {
+            return <div key={course.title}>{course.title}</div>;
+          })}
+          <input type="submit" value="Save" />
         </form>
       </div>
     );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => {
+  return {
+    courses: state.courses
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  createCourse: course => dispatch(createCourse(course))
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./components/App";
+import configureStore from "./redux/configureStore";
+
+const store = configureStore();
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("app")
 );
