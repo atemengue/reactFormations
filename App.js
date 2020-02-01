08b601906f2335dd82d1f1537be80040047ea4ad
atemengue <moafembe@gmail.com>
2020-02-01 23:30:59 +0100

    initial commit add createContext

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import indexScreen from "./src/screens/indexScreen";
+import { BlogProvider } from "./src/context/BlogContext";
+
+const navigator = createStackNavigator(
+  {
+    Index: indexScreen
+  },
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Blogs"
+    }
+  }
+);
+
+//Wrapper app change default
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
---
+++ src/screens/indexScreen.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import BlogContext from "../context/BlogContext";
+
+const indexScreen = () => {
+  const blogPosts = useContext(BlogContext);
+
+  return (
+    <View>
+      <Text>Index View</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default indexScreen;
